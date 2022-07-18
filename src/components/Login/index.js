ab329bf4c766ccc86dab93c50c601e300871dfd6
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-07-18 19:55:34 +0300

    Added layout and adaptive notific

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SignUpForm from './Form/SignUpForm';
 import LoginForm from './Form/LoginForm';
+import Notific from '../Notific';
 
 import styles from './Login.module.scss';
 
@@ -8,16 +9,27 @@ import loginBg from './login-bg.jpg';
 
 const Login = () => {
     const [login, setLogin] = React.useState(false);
+    const [notofic, setNotific] = React.useState(true);
+    const [notificContent, setNotificContent] = React.useState(true);
 
     return (
         <section className={styles.login}>
+            <div className={styles.notificContainer}>
+                {notofic &&
+                    <Notific
+                        title={notificContent ? "SUCCESS" : "FAILED"}
+                        text={notificContent ? "You can Log-in now." : "Sign-up failed. Please try again."}
+                        color={notificContent ? "#6EEB83" : "#FF5E5B"}
+                    />
+                }
+            </div>
             <div className={styles.image}>
                 <h3 className={styles.title}>Sign Up</h3>
-                <img src={loginBg} alt="background login"/>
+                <img src={loginBg} alt="background login" />
             </div>
             <div className={styles.content}>
                 <div className={styles.inner}>
-                    { login ? <LoginForm /> : <SignUpForm /> }
+                    {login ? <LoginForm /> : <SignUpForm />}
                 </div>
             </div>
         </section>
---
+++ src/components/Notific/index.js
@@ -0,0 +1,18 @@
+import styles from './Notific.module.scss';
+
+import Icons from '../Icons';
+
+const Notific = ({title,text,color}) => {
+    return (
+        <div className={styles.box} style={{background:color}}>
+            <p><span>{title}</span>
+                {text}
+            </p>
+            <div className={styles.iconContainer}>
+                <Icons name="notific" />
+            </div>
+        </div>
+    )
+}
+
+export default Notific;
