a767f20531274a08b5d6680b86e36194deda0f8c
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-10-16 16:09:26 +0300

    Added HashRouter fot gh-pages

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import 'normalize.css';
 import './index.scss';
 import './firebase';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </>,
 );
