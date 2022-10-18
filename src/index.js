bab82b21fa4ee5e58daf03097ed2dfb0f8f808cb
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-10-18 20:40:22 +0300

    Deleted gh-pages plugin

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import 'normalize.css';
 import './index.scss';
 import './firebase';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </>,
 );
